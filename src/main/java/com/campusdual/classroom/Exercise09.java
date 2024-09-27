83a580c72dc75bf05c96739419bfd0643f5d66ad
Juan Fuente <127140423+Juan-Fuente-T@users.noreply.github.com>
2024-09-27 13:40:28 +0200

    Resuelto ejercicio 9

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,7 +3,7 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
-
+        doWhileLoop(9);
     }
 
     //TODO ↓
@@ -12,6 +12,18 @@ public class Exercise09 {
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
+        int x = 0;
 
+        do{
+            System.out.println("Entra con valor: " + x + ", sale con valor: " + (x + 1));
+            x++;
+        }while (x < num);
     }
 }
+
+/*Desde el método main() se llamará al método doWhileLoop() pasándole un valor por parámetro.
+
+El método doWhileLoop() recibe un número entero por parámetro. Dentro del método, utilizando un bucle do-while,
+ se llevarán a cabo comprobaciones cíclicas partiendo de cero ("X"). Mientras que "X" sea menor que el parámetro
+ recibido, se mostrará primero un mensaje que indique con qué valor entra al bucle. Luego se aumentará "X" en una
+  unidad y se mostrará otro mensaje que indique con qué valor saldrá "X" del bucle.*/
